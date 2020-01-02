06d4d6fb3fa1cec73a950e269d31d6d5666fc319
banthi <lchijuwa@gmail.com>
2020-01-02 11:26:45 +0200

    deals with images

---
+++ app/src/main/java/com/banthi/androiddemos/Images.java
@@ -0,0 +1,14 @@
+package com.banthi.androiddemos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Images extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+    }
+}
